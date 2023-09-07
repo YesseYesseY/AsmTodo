cb3521a93487d5366d1b8326c9c6dc7fce5c9138
YesseYesseY <yesseyyesyessey@gmail.com>
2023-09-07 14:30:51 +0200

    int_to_string and reverse_string complete

--- test.asm
+++ test.asm
@@ -1,18 +1,15 @@
 global _start
 
 section .data
-    string db 0 dup(4)
-
-    test_str db "yesseY"
+    string db 4 dup(0)
     new_line db 0xA
 
 section .text
 _start:
     mov eax, 1337
     mov ebx, string
-    mov esi, 0
     call int_to_str
-    
+
     mov eax, 4
     mov ebx, 1
     mov ecx, string
@@ -29,7 +26,25 @@ _start:
     mov ebx, 0
     int 0x80
 
+; eax = str
+; ebx = str_len
 reverse_string:
+    mov esi, eax         ; ptr to start of string
+    mov edi, eax         
+    add edi, ebx         ; ptr to end of string
+    sub edi, 1
+reverse_string_loop:
+    mov cl, [esi]        ; first char
+    mov dl, [edi]        ; last char
+
+    mov [edi], cl
+    mov [esi], dl
+
+    inc esi
+    dec edi
+
+    cmp esi, edi
+    jle reverse_string_loop
     ret
 
 ; eax = num
@@ -41,9 +56,12 @@ int_to_str_loop:
     xor edx, edx          ; Clear edx, otherwise get Floating point exception
     div ecx               ; Divide num by 10
     add edx, 48           ; Add 48 to remainder, this will give the ASCII value of the number
-    mov [ebx+esi], edx    ; Add the char to string
+    mov [ebx+esi], dl     ; Add the char to string
     inc esi               ; i++
     cmp eax, 0
     jg int_to_str_loop    ; if (num > 0) goto int_to_str_loop
-    ; TODO: Reverse string
+
+    mov eax, string
+    mov ebx, esi
+    call reverse_string
     ret
