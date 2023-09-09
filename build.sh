d7104232b4df6d309a56b968c30957bbe712d5a3
YesseYesseY <yesseyyesyessey@gmail.com>
2023-09-09 12:35:11 +0200

    string_to_int complete! :D

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 nasm -f elf32 hello.asm -o hello.o
-nasm -f elf32 test.asm -o test.o
+nasm -f elf32 test.asm -F dwarf -o test.o
 ld -m elf_i386 hello.o -o hello
 ld -m elf_i386 test.o -o test
--- test.asm
+++ test.asm
@@ -2,11 +2,16 @@ global _start
 
 section .data
     string db 4 dup(0)
+    string_2 db "1337"
     new_line db 0xA
 
 section .text
 _start:
-    mov eax, 1337
+    mov esi, string_2
+    mov ecx, 4
+    call string_to_int
+
+    mov eax, ebx
     mov ebx, string
     call int_to_str
 
@@ -64,4 +69,29 @@ int_to_str_loop:
     mov eax, string
     mov ebx, esi
     call reverse_string
+    ret
+
+; esi = str
+; ecx = str_len
+; return ebx
+string_to_int:
+    xor edx, edx
+    mov edi, esi
+    add edi, ecx
+    sub edi, 1        ; edi = ptr to end of string
+    mov ecx, 1
+string_to_int_loop:
+    movzx eax, byte [edi]     
+    sub eax, byte '0'
+    mul ecx
+    add ebx, eax
+
+    mov eax, ecx
+    mov ecx, 10
+    mul ecx
+    mov ecx, eax
+    
+    dec edi     ; dec the end_of_str ptr
+    cmp edi, esi
+    jge string_to_int_loop ; if the ptr is greater than start of string then loop
     ret
